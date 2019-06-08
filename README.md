280647bb9d832a48bce5c72075a4ac5e7c318288
Genadi Ivanov <genadi.ivanov89@gmail.com>
2019-06-08 21:00:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# httpclient-devbg
+Example code snippets for HTTPClient API in Java 11 DevBg
---
+++ src/main/java/com/example/httpclient/HttpClientExamples.java
@@ -0,0 +1,9 @@
+package com.example.httpclient;
+
+
+public class HttpClientExamples {
+
+    public static void main(String[] args) {
+        System.out.println("Example code snippets for HTTPClient API in Java 11 DevBg");
+    }
+}
